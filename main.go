21abfd6b6331e0d93364df24061e16cc0e7ae57e
Hatesnake69 <vladimir@itsy.dev>
2022-11-26 16:47:52 +0300

    add Models

--- main.go
+++ main.go
@@ -3,7 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
+
+type Apartment struct {
+	ID                 uint
+	Name               string
+	Description        string
+	RoomCount          uint
+	SleepingPlaceCount uint
+}
+
+type Booking struct {
+	ID            uint
+	Name          uint
+	PhoneNumber   string
+	ArrivalTime   time.Time
+	DepartureTime time.Time
+	ApartmentID   Apartment
+	Status        string
+}
+
+type Pictures struct {
+	ID       uint
+	FileName string
+}
 
 func main() {
 	http.HandleFunc("/", HelloServer)
